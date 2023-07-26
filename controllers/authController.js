5023508f049346dfa87befbc452324c1cfc9d654
Gihan-Madhusankha <gihanmadhusankha1999@gmail.com>
2023-07-26 20:28:18 +0530

    update content

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ import userModel from "../models/userModel.js";
 // import orderModel from "../models/orderModel.js";
 
 // import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
-import { hashPassword } from "./../helpers/authHelper.js";
+import {comparePassword, hashPassword} from "./../helpers/authHelper.js";
 import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
@@ -80,16 +80,16 @@ export const loginController = async (req, res) => {
         if (!user) {
             return res.status(404).send({
                 success: false,
-                message: "Email is not registerd",
+                message: "Email is not registered",
             });
         }
-        // const match = await comparePassword(password, user.password);
-        // if (!match) {
-        //     return res.status(200).send({
-        //         success: false,
-        //         message: "Invalid Password",
-        //     });
-        // }
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return res.status(200).send({
+                success: false,
+                message: "Invalid Password",
+            });
+        }
         //token
         const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
             expiresIn: "7d",
